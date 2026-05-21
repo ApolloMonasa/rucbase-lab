6e68a19eb885ce4eadd72c608edeeec6f2771862
ApolloMonasa <2784761245@qq.com>
2026-05-21 20:40:52 +0800

    Implement TransactionManager begin, commit, and abort
    begin(): create new Transaction with unique ID and timestamp,
    set state to GROWING, register in global txn_map.
    
    commit(): set SHRINKING state, release all locks via lock_set_,
    clear write_set_, set COMMITTED state.
    
    abort(): reverse all write operations by popping from write_set_
    in reverse order — undo INSERT by deleting, DELETE by re-inserting,
    UPDATE by restoring old value. Then release all locks and set
    ABORTED state.

--- src/transaction/transaction_manager.cpp
+++ src/transaction/transaction_manager.cpp
@@ -21,13 +21,23 @@ std::unordered_map<txn_id_t, Transaction *> TransactionManager::txn_map = {};
  * @param {LogManager*} log_manager 日志管理器指针
  */
 Transaction * TransactionManager::begin(Transaction* txn, LogManager* log_manager) {
-    // Todo:
     // 1. 判断传入事务参数是否为空指针
     // 2. 如果为空指针，创建新事务
     // 3. 把开始事务加入到全局事务表中
     // 4. 返回当前事务指针
-    
-    return nullptr;
+    std::scoped_lock lock{latch_};
+
+    if (txn == nullptr) {
+        txn_id_t txn_id = next_txn_id_++;
+        timestamp_t start_ts = next_timestamp_++;
+        txn = new Transaction(txn_id);
+        txn->set_start_ts(start_ts);
+        txn->set_state(TransactionState::GROWING);
+    }
+
+    TransactionManager::txn_map[txn->get_transaction_id()] = txn;
+
+    return txn;
 }
 
 /**
@@ -36,13 +46,26 @@ Transaction * TransactionManager::begin(Transaction* txn, LogManager* log_manage
  * @param {LogManager*} log_manager 日志管理器指针
  */
 void TransactionManager::commit(Transaction* txn, LogManager* log_manager) {
-    // Todo:
     // 1. 如果存在未提交的写操作，提交所有的写操作
     // 2. 释放所有锁
     // 3. 释放事务相关资源，eg.锁集
     // 4. 把事务日志刷入磁盘中
     // 5. 更新事务状态
 
+    txn->set_state(TransactionState::SHRINKING);
+
+    // 释放所有锁
+    auto lock_set = txn->get_lock_set();
+    for (auto &lock_data_id : *lock_set) {
+        lock_manager_->unlock(txn, lock_data_id);
+    }
+    lock_set->clear();
+
+    // 清空写操作集
+    txn->get_write_set()->clear();
+
+    // 更新事务状态
+    txn->set_state(TransactionState::COMMITTED);
 }
 
 /**
@@ -51,11 +74,46 @@ void TransactionManager::commit(Transaction* txn, LogManager* log_manager) {
  * @param {LogManager} *log_manager 日志管理器指针
  */
 void TransactionManager::abort(Transaction * txn, LogManager *log_manager) {
-    // Todo:
     // 1. 回滚所有写操作
     // 2. 释放所有锁
     // 3. 清空事务相关资源，eg.锁集
     // 4. 把事务日志刷入磁盘中
     // 5. 更新事务状态
-    
+
+    // 回滚所有写操作：逆序遍历 write_set，撤销每个写操作
+    auto write_set = txn->get_write_set();
+    while (!write_set->empty()) {
+        WriteRecord *write_record = write_set->back();
+        write_set->pop_back();
+
+        TabMeta &tab = sm_manager_->db_.get_table(write_record->GetTableName());
+        auto &tab_name = write_record->GetTableName();
+        RmFileHandle *fh = sm_manager_->fhs_.at(tab_name).get();
+
+        switch (write_record->GetWriteType()) {
+            case WType::INSERT_TUPLE:
+                // 撤销插入：删除刚插入的记录
+                fh->delete_record(write_record->GetRid(), nullptr);
+                break;
+            case WType::DELETE_TUPLE:
+                // 撤销删除：重新插入被删除的记录
+                fh->insert_record(write_record->GetRid(), write_record->GetRecord().data);
+                break;
+            case WType::UPDATE_TUPLE:
+                // 撤销更新：恢复旧值
+                fh->update_record(write_record->GetRid(), write_record->GetRecord().data, nullptr);
+                break;
+        }
+        delete write_record;
+    }
+
+    // 释放所有锁
+    auto lock_set = txn->get_lock_set();
+    for (auto &lock_data_id : *lock_set) {
+        lock_manager_->unlock(txn, lock_data_id);
+    }
+    lock_set->clear();
+
+    // 更新事务状态
+    txn->set_state(TransactionState::ABORTED);
 }
